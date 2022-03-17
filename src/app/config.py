11fc70f7aa5dd64e10c9f882a75422497fee6d97
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2022-03-17 09:22:38 +0300

    Update config.py

--- src/app/config.py
+++ src/app/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     authjwt_secret_key: str = "sercet"
     postgres_url: str = "postgresql+psycopg2://postgres:000000@localhost/dpdb"
+    timeout: int = 5
 
     class Config:
         env_file = '.env'
