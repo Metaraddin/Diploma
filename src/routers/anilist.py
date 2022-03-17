ab5399b6fa8517ff0ac1e2f077295893a558bb89
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2022-03-17 10:54:29 +0300

    Update anilist.py

--- src/routers/anilist.py
+++ src/routers/anilist.py
@@ -28,7 +28,7 @@ async def get_current_user_id(anilist_access_token: Optional[str] = Cookie(None)
 
 
 @router.get('/rec', status_code=200)
-async def get_rec(anilist_access_token: Optional[str] = Cookie(None)):
+async def get_rec(page: int = 1, per_page: int = 50, anilist_access_token: Optional[str] = Cookie(None)):
     query = '''
     query ($page: Int, $perPage: Int) {
         Page (page: $page, perPage: $perPage) {
@@ -50,12 +50,16 @@ async def get_rec(anilist_access_token: Optional[str] = Cookie(None)):
         }
     }
     '''
+    variables = {
+        'page': page,
+        'perPage': per_page
+    }
 
     url = 'https://graphql.anilist.co'
     headers = {
         'Authorization': "Bearer " + anilist_access_token
     }
 
-    response = requests.post(url, json={'query': query}, headers=headers)
+    response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers)
     return response.json()
 
