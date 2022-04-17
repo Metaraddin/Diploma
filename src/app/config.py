80c97b25b900f37856b426796fe4b084b738f717
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2022-04-17 13:10:49 +0300

    upd

--- src/app/config.py
+++ src/app/config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     authjwt_secret_key: str = "sercet"
     postgres_url: str = "postgresql+psycopg2://postgres:000000@localhost/dpdb"
     timeout: int = 5
+    client_secret: str = '0m9cgoidK2yW7tO01yYQRixLdmk2eptGmzgOD7G7'
+    client_id: str = '7698'
+    redirect_uri: str = 'http://localhost:8000/anilist/auth/callback'
 
     class Config:
         env_file = '.env'
--- src/routers/user.py
+++ src/routers/user.py
@@ -7,12 +7,59 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from src.app.dependencies import get_db
-from src.models.user import UserOut, UserUpdate, UserCreate
+from src.models.user import UserOut, UserUpdate, UserCreate, UserLogin
 from src.models.general import UserToken
 from src.repositories import user, avatar
+from src.models.token import Token
+
 
 router = APIRouter(prefix="/user", tags=["User"])
 security = HTTPBearer()
+
+
+@router.post("/", status_code=200, response_model=UserToken)
+async def create_user(user_info: UserCreate, session: Session = Depends(get_db),
+                      Authorize: AuthJWT = Depends()):
+    """
+    Создаёт нового пользователя и возвращает его вместе с access token.
+    """
+    curr_user = user.create_user(u=user_info, s=session)
+    if not curr_user:
+        raise HTTPException(status_code=400, detail=[{'msg': 'User with this email already exists'}])
+    token = user.create_user_token(curr_user.id, Authorize)
+    return UserToken(User=curr_user, Token=token)
+
+
+@router.post("/login", status_code=200, response_model=Token)
+async def login(user_info: UserLogin, session: Session = Depends(get_db), Authorize: AuthJWT = Depends()):
+    """
+    Возвращает access token пользователя в случае успешной проверки.
+    """
+    curr_user = user.get_user_by_email(email=user_info.email, s=session)
+    if not curr_user:
+        raise HTTPException(status_code=400, detail=[{'msg': 'User with this email does not exist'}])
+    if not user.validate_password(password=user_info.password, hashed_password=curr_user.hashed_password):
+        raise HTTPException(status_code=400, detail=[{'msg': 'Incorrect email or password'}])
+    return user.create_user_token(id=curr_user.id, Authorize=Authorize)
+
+
+@router.get("/{id}", status_code=200, response_model=UserOut)
+async def get_user(id: int, session: Session = Depends(get_db)):
+    """
+    Возвращает пользователя по **user.id**
+    """
+    curr_user = user.get_user_by_id(id, session)
+    if not curr_user:
+        raise HTTPException(status_code=400, detail=[{'msg': 'User with this id does not exist'}])
+    return curr_user
+
+
+@router.get("/all", status_code=200, response_model=List[UserOut])
+async def get_all_user(limit: int = 100, skip: int = 0, session: Session = Depends(get_db)):
+    """
+    Возвращает список всех пользователей.
+    """
+    return user.get_all_user(session, limit, skip)
 
 
 @router.get("/curr", status_code=200, response_model=UserOut)
@@ -53,39 +100,7 @@ async def edit_current_user(user_info: UserUpdate, session: Session = Depends(ge
     return curr_user
 
 
-@router.get("/", status_code=200, response_model=UserOut)
-async def get_user(id: int, session: Session = Depends(get_db)):
-    """
-    Возвращает пользователя по **user.id**
-    """
-    curr_user = user.get_user_by_id(id, session)
-    if not curr_user:
-        raise HTTPException(status_code=400, detail=[{'msg': 'User with this id does not exist'}])
-    return curr_user
-
-
-@router.post("/", status_code=200, response_model=UserToken)
-async def create_user(user_info: UserCreate, session: Session = Depends(get_db),
-                      Authorize: AuthJWT = Depends()):
-    """
-    Создаёт нового пользователя и возвращает его вместе с access token.
-    """
-    curr_user = user.create_user(u=user_info, s=session)
-    if not curr_user:
-        raise HTTPException(status_code=400, detail=[{'msg': 'User with this email already exists'}])
-    token = user.create_user_token(curr_user.id, Authorize)
-    return UserToken(User=curr_user, Token=token)
-
-
-@router.get("/all", status_code=200, response_model=List[UserOut])
-async def get_all_user(limit: int = 100, skip: int = 0, session: Session = Depends(get_db)):
-    """
-    Возвращает список всех пользователей.
-    """
-    return user.get_all_user(session, limit, skip)
-
-
-@router.get("/avatar", status_code=200, response_model=UserOut)
+@router.get("/curr/avatar", status_code=200, response_model=UserOut)
 async def get_avatar(user_id: int, session: Session = Depends(get_db)):
     """
     Возвращает аватар пользователя по **user.id**.
@@ -97,7 +112,7 @@ async def get_avatar(user_id: int, session: Session = Depends(get_db)):
     return Response(content=curr_avatar.file, media_type='image/png')
 
 
-@router.patch("/avatar", status_code=200, response_model=UserOut)
+@router.patch("/curr/avatar", status_code=200, response_model=UserOut)
 async def edit_current_user_avatar(image: bytes = File(...), session: Session = Depends(get_db),
                       Authorize: AuthJWT = Depends(), auth: HTTPAuthorizationCredentials = Security(security)):
     """
@@ -109,7 +124,7 @@ async def edit_current_user_avatar(image: bytes = File(...), session: Session = 
     return user.edit_avatar_id(user_id=int(Authorize.get_jwt_subject()), avatar_id=mapper.id, s=session)
 
 
-@router.delete("/avatar", status_code=200, response_model=UserOut)
+@router.delete("/curr/avatar", status_code=200, response_model=UserOut)
 async def delete_current_user_avatar(session: Session = Depends(get_db),
                                      Authorize: AuthJWT = Depends()):
     """
